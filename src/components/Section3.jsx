6765ca1c76665c26e258f1e2b843de482610e9d3
raphaelohai5 <sameleno6@gmail.com>
2023-06-05 21:09:38 +0100

    brands fixed

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,6 +1,6 @@
 import { Grid, Stack, Typography } from "@mui/material";
 // import React from "react";
-import { Center } from ".";
+import { Center, HideMobile } from ".";
 
 const Section3 = () => {
   return (
@@ -9,32 +9,32 @@ const Section3 = () => {
         Trusted by the word's leading organizations
       </Typography>
       <Grid container spacing={1}>
-        <Grid item lg={2} xs={6}>
+        <Grid item lg={2} xs={6} sm={2}  sx={HideMobile}>
           <Center pr={2}>
             <img className="responsive" src="https://github.githubassets.com/images/modules/site/home-campaign/logos/stripe.svg" />
           </Center>
         </Grid>
-        <Grid item lg={2} xs={6}>
+        <Grid item lg={2} xs={6} sm={2}>
           <Center pl={4}>
             <img className="responsive" src="https://github.githubassets.com/images/modules/site/home-campaign/logos/pinterest.svg"/>
           </Center>
         </Grid>
-        <Grid item lg={2} xs={6}>
+        <Grid item lg={2} xs={6} sm={2} sx={HideMobile}>
           <Center pr={2}>
             <img className="responsive" src="https://github.githubassets.com/images/modules/site/home-campaign/logos/kpmg.svg"/>
           </Center>
         </Grid>
-        <Grid item lg={2} xs={6}>
+        <Grid item lg={2} xs={6} sm={2}>
           <Center pl={5}>
             <img className="responsive" src="https://github.githubassets.com/images/modules/site/home-campaign/logos/mercedes.svg" />
           </Center>
         </Grid>
-        <Grid item lg={2} xs={6}>
+        <Grid item lg={2} xs={6} sm={2}  sx={HideMobile}>
           <Center pr={2}>
             <img className="responsive" src="https://github.githubassets.com/images/modules/site/home-campaign/logos/pg.svg" />
           </Center>
         </Grid>
-        <Grid item lg={2} xs={6}>
+        <Grid item lg={2} xs={6} sm={2}>
           <Center pl={2}>
             <img className="responsive" src="https://github.githubassets.com/images/modules/site/home-campaign/logos/telus.svg" />
           </Center>
